b26b3c577ee4d091e72a08115a0285010009a4ea
loglmd <az9471@sina.cn>
2019-07-10 18:22:14 +0800

    update selectArea

--- src/pages/components/SelectArea.tsx
+++ src/pages/components/SelectArea.tsx
@@ -2,15 +2,23 @@
 import React from 'react';
 import { connect } from "dva";
 import { InputNumber } from 'antd';
-
+import { CONFIG_LABEL } from "../config/config";
+// 不允许为空，没有初始cp的不让调
+// 允许小数
 @connect(({ file }: any) => ({ ...file }))
 export default class SelectArea extends React.Component<any, any>{
     render() {
-        const { areaname, cpposition } = this.props;
+        const { config,areaname, cpposition } = this.props;
+        const numprops = (ind) => ({
+            step: config[CONFIG_LABEL.MOVE_GAP],
+            value:cpposition[ind]||"",
+        });
         return areaname && areaname.length ? (
             <div>
                 <span>{areaname}：</span>
+                <span>x：</span>
                 <InputNumber value={cpposition[0]} />
+                <span>y：</span>
                 <InputNumber value={cpposition[1]} />
             </div>
         ) : null;
