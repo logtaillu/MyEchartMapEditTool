d7077ffdb73382a51812b2eeaf480476dcfe43fb
logtaillu <logtaillu@users.noreply.github.com>
2019-07-15 11:04:27 +0800

    update readme&rename

--- .umirc.ts
+++ .umirc.ts
@@ -3,15 +3,15 @@ import { IConfig } from 'umi-types';
 // ref: https://umijs.org/config/
 const config: IConfig =  {
   treeShaking: true,
-  base: '/MyMapTest/',
-  publicPath: '/MyMapTest/',
+  base: '/MyEchartMapEditTool/',
+  publicPath: '/MyEchartMapEditTool/',
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
       antd: true,
       dva: true,
       dynamicImport: false,
-      title: 'MyMapTest',
+      title: 'MyEchartMapEditTool',
       dll: false,
       
       routes: {
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# MyMapTest
-地图文字显示测试
-根据地图情况，还是需要目测调整
-1. 导入地图，设置地图长宽、文字大小、步距
-2. 显示地图
-3. 选择后键盘移动位置(没有cp的暂时不能进行移动)
+# MyEchartMapEditTool
+导入地图geojson查看在echarts中的效果，手动调整文字和文字位置并导出结果，方便文字重叠调整
+功能：  
+1. 导入地图，设置地图长宽、文字大小等查看显示效果
+2. 简单展示echarts地图显示效果
+3. 选择后键盘移动位置(没有cp的目前不允许进行移动)
 4. 导出文件
-5. 下载转码压缩[copy from echarts-map-tools]  
+5. 压缩[copy from echarts-map-tools]，在echarts中会被解码，在其他geojson工具中不一定能识别
